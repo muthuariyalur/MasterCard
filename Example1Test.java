9ee1236dd7cc6018eb9d0dc3b946833a314c9223
muthuariyalur <50877932+muthuariyalur@users.noreply.github.com>
2019-05-21 23:03:37 +0800

    Add files via upload
    Initial Version of Test class

---
+++ Example1Test.java
@@ -0,0 +1,20 @@
+package JUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Example1Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		//Testing the main function
+	}
+
+}
---
+++ Example2Test.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package JUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author MAlangaram
+ *
+ */
+public class Example2Test {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link test.Example2#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		 //Testing the main
+	}
+
+}
